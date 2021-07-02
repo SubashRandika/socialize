91ffe7af60cef44d30d8240dd6d53a8c8e915174
SubashRandika <subash.rodrigo@gmail.com>
2021-07-02 20:28:07 +0530

    Created NavBar component with initial home, login and register pages using Sematic React UI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NavBar from "./components/NavBar";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 const App = () => {
 	return (
-		<div>
-			<h1>Socialize</h1>
-		</div>
+		<Router>
+			<Container>
+				<NavBar />
+				<Route exact path='/' component={Home} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+			</Container>
+		</Router>
 	);
 };
 
