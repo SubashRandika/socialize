6a2053a76b37e8e04a066fea61a32cd49782c445
SubashRandika <subash.rodrigo@gmail.com>
2021-07-02 22:06:05 +0530

    Implemented home page recent posts card UI with loading. Used dayjs to format posted date time.

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Card, Icon, Image, Label } from "semantic-ui-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+const PostCard = ({ post }) => {
+	const { id, body, username, likes, likeCount, commentCount, createdAt } = post;
+
+	const handleLikePost = () => {
+		console.log("Post Liked");
+	};
+
+	const handleCommentPost = () => {
+		console.log("Post Commented");
+	};
+
+	return (
+		<Card fluid>
+			<Card.Content>
+				<Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.png' />
+				<Card.Header>{username}</Card.Header>
+				<Card.Meta as={Link} to={`/posts/${id}`}>
+					{dayjs().diff(dayjs(createdAt), "d") >= 1
+						? dayjs(createdAt).format("MMMM D, YYYY h:mm a")
+						: dayjs(createdAt).fromNow(true)}
+				</Card.Meta>
+				<Card.Description>{body}</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<Button as='div' labelPosition='right' onClick={handleLikePost}>
+					<Button basic color='teal'>
+						<Icon name='heart' />
+						Like
+					</Button>
+					<Label basic color='teal' pointing='left'>
+						{likeCount}
+					</Label>
+				</Button>
+				<Button as='div' labelPosition='right' onClick={handleCommentPost}>
+					<Button basic color='blue'>
+						<Icon name='comments' />
+						Comment
+					</Button>
+					<Label basic color='blue' pointing='left'>
+						{commentCount}
+					</Label>
+				</Button>
+			</Card.Content>
+		</Card>
+	);
+};
+
+export default PostCard;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Grid, Header, Loader } from "semantic-ui-react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
+	const [posts, setPosts] = useState([]);
+	const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+	useEffect(() => {
+		if (!loading) {
+			const { getPosts } = data;
+			setPosts(getPosts);
+		}
+	}, [data, loading]);
+
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
-	)
-}
+		<>
+			<Header as='h1'>Recent Posts</Header>
+			<Grid columns={3}>
+				{loading ? (
+					<Loader active={loading} size='big'>
+						Loading...
+					</Loader>
+				) : (
+					posts?.map((post) => (
+						<Grid.Column key={post.id}>
+							<PostCard post={post} />
+						</Grid.Column>
+					))
+				)}
+			</Grid>
+		</>
+	);
+};
 
-export default Home
+const FETCH_POSTS_QUERY = gql`
+	{
+		getPosts {
+			id
+			body
+			username
+			likes {
+				username
+			}
+			likeCount
+			comments {
+				id
+				body
+				username
+				createdAt
+			}
+			commentCount
+			createdAt
+		}
+	}
+`;
 
+export default Home;
