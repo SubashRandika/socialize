d65c983c1d9b87a513500cd51eacd40fcb97b226
SubashRandika <subash.rodrigo@gmail.com>
2021-07-03 21:52:46 +0530

    Created Login form component with semantic UI.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Button, Container, Form, Grid, Header, Input } from "semantic-ui-react";
 
 const Login = () => {
-	return (
-		<div>
-			<h1>Login</h1>
-		</div>
-	)
-}
+	const [errors, setErrors] = useState({});
+	const [values, setValues] = useState({
+		username: "",
+		password: ""
+	});
+	const history = useHistory();
 
-export default Login
+	const handleChange = (event) => {
+		setValues({
+			...values,
+			[event.target.name]: event.target.value
+		});
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<Container>
+			<Header as='h1' textAlign='center'>
+				Login
+			</Header>
+			<Grid centered columns={3}>
+				<Grid.Column>
+					<Form onSubmit={handleSubmit} noValidate>
+						<Form.Field
+							control={Input}
+							type='text'
+							label='Username'
+							placeholder='Username'
+							name='username'
+							value={values.username}
+							onChange={handleChange}
+							error={errors.username ? errors.username : false}
+						/>
+						<Form.Field
+							control={Input}
+							type='password'
+							label='Password'
+							placeholder='Password'
+							name='password'
+							value={values.password}
+							onChange={handleChange}
+							error={errors.password ? errors.password : false}
+						/>
+						<Button primary type='submit'>
+							Login
+						</Button>
+					</Form>
+				</Grid.Column>
+			</Grid>
+		</Container>
+	);
+};
+
+export default Login;
