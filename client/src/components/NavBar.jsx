57fef5103c1ba102f8e16f0cea83c1ea8cbc230f
SubashRandika <subash.rodrigo@gmail.com>
2021-07-04 21:17:19 +0530

    Implemented show logout when signedin and show login, register when not signedin based on localstorage persisted token. Used jwt-decode for decode jwt token.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { AuthContext } from "../context/auth";
 
 const NavBar = () => {
 	const { pathname } = useLocation();
 	const path = pathname === "/" ? "home" : pathname.substr(1);
 	const [activeItem, setActiveItem] = useState(path);
+	const { user, logout } = useContext(AuthContext);
 
 	const handleItemClick = (e, { name }) => {
 		setActiveItem(name);
 	};
 
-	return (
+	const navBar = user ? (
+		<Menu pointing secondary size='massive' color='teal'>
+			<Menu.Item name={user.username} active as={Link} to='/' />
+			<Menu.Menu position='right'>
+				<Menu.Item name='logout' onClick={logout} />
+			</Menu.Menu>
+		</Menu>
+	) : (
 		<Menu pointing secondary size='massive' color='teal'>
 			<Menu.Item name='home' active={activeItem === "home"} onClick={handleItemClick} as={Link} to='/' />
 			<Menu.Menu position='right'>
@@ -26,6 +35,8 @@ const NavBar = () => {
 			</Menu.Menu>
 		</Menu>
 	);
+
+	return navBar;
 };
 
 export default NavBar;
