ed15f79de5ad9fc269a402e75c60b5990bd1ec44
SubashRandika <subash.rodrigo@gmail.com>
2021-07-15 21:54:41 +0530

    Altered deployement setup to support graphql backend and react frontend.

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ const startServer = async () => {
 	try {
 		if (process.env.NODE_ENV === "production") {
 			const __dirname = path.resolve();
-			app.use(express.static("client/build"));
+			app.use(express.static("public"));
 
-			app.get("/", (req, res) => {
-				res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+			app.get("*", (req, res) => {
+				res.sendFile(path.resolve(__dirname, "public", "index.html"));
 			});
 		}
 
