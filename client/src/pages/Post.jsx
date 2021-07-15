8638a092b088a7864415e174195a0516d5e4f3e8
SubashRandika <subash.rodrigo@gmail.com>
2021-07-15 19:54:49 +0530

    Implemented create comment form with functionality.

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,22 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
-import { Button, Card, Grid, Icon, Image, Label, Loader, Popup } from "semantic-ui-react";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { Button, Card, Form, Grid, Header, Icon, Image, Input, Label, Loader, Popup } from "semantic-ui-react";
 import LikeButton from "../components/LikeButton";
 import DeleteButton from "../components/DeleteButton";
 import { AuthContext } from "../context/auth";
 
 dayjs.extend(relativeTime);
 const Post = () => {
+	const [comment, setComment] = useState("");
 	const { postId } = useParams();
 	const history = useHistory();
 	const { user } = useContext(AuthContext);
 
 	const { loading, data } = useQuery(FETCH_POST, {
 		variables: { postId }
+	});
+
+	const [createComment] = useMutation(CREATE_COMMENT, {
+		update() {
+			setComment("");
+		},
+		variables: { postId, body: comment }
 	});
 
 	const deletePostCallback = () => {
@@ -74,6 +82,29 @@ const Post = () => {
 								{user?.username === username && <DeleteButton postId={postId} callback={deletePostCallback} />}
 							</Card.Content>
 						</Card>
+						{user && (
+							<Card fluid>
+								<Card.Content>
+									<Header as='h4'>Type a your comment</Header>
+									<Form>
+										<Form.Group>
+											<Form.Field
+												width={14}
+												control={Input}
+												type='text'
+												placeholder='Comment here...'
+												name='comment'
+												value={comment}
+												onChange={(event) => setComment(event.target.value)}
+											/>
+											<Button type='submit' color='teal' disabled={comment.trim() === ""} onClick={createComment}>
+												Comment
+											</Button>
+										</Form.Group>
+									</Form>
+								</Card.Content>
+							</Card>
+						)}
 						{comments.map((comment) => (
 							<Card fluid key={comment?.id}>
 								<Card.Content>
@@ -115,6 +146,21 @@ const FETCH_POST = gql`
 			}
 			commentCount
 			createdAt
+		}
+	}
+`;
+
+const CREATE_COMMENT = gql`
+	mutation ($postId: ID!, $body: String!) {
+		createComment(postId: $postId, body: $body) {
+			id
+			comments {
+				id
+				body
+				username
+				createdAt
+			}
+			commentCount
 		}
 	}
 `;
